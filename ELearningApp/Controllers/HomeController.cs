4323d6c8a2bb81358ae141e2645ba333f5ef1253
Ahmed Yasser <ahmedbkar11@gmail.com>
2024-10-09 01:43:29 +0300

    First Try

---
+++ ELearningApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ELearningApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> GetCategories()
+        {
+            return Json("");
+        }
+    }
+}
